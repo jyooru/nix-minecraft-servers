6a38880d4dad55bd8126400652114ae4c8083ba1
Joel <joel@joel.tokyo>
2021-12-10 18:10:14 +1000

    feat: implement getting server url and sha1

--- nixpkgs_minecraft/__init__.py
+++ nixpkgs_minecraft/__init__.py
@@ -1,10 +1,17 @@
 from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Dict
+from typing import Any, Dict
 
 import requests
 from dataclasses_json import DataClassJsonMixin, LetterCase, config
 from marshmallow import fields
+
+
+@dataclass
+class Download(DataClassJsonMixin):
+    sha1: str
+    size: int
+    url: str
 
 
 @dataclass
@@ -27,6 +34,21 @@ class Version(DataClassJsonMixin):
             letter_case=LetterCase.CAMEL,
         )
     )
+
+    def get_manifest(self) -> Any:
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.json()
+
+    def get_downloads(self) -> Dict[str, Download]:
+        manifest = self.get_manifest()
+        return {
+            key: Download.from_dict(value)
+            for key, value in manifest["downloads"].items()
+        }
+
+    def get_server(self) -> Download:
+        return self.get_downloads()["server"]
 
 
 def get_versions() -> Dict[str, Version]:
