f42b6e097bc40906e4dd9f1cf11f3ec922d1bad1
Joel <joel@joel.tokyo>
2021-12-18 14:32:33 +1000

    feat: create flake and devshell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Minecraft server packages";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        with pkgs;
+        rec      {
+          devShell = pkgs.mkShell {
+            packages = [ python3 ] ++ (with python3Packages; [ requests dataclasses-json ]);
+          };
+        }
+      );
+}
