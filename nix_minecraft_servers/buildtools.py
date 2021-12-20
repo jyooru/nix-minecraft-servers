6114393083fbb9d0eaed043477e54761d0261dea
Joel <joel@joel.tokyo>
2021-12-20 15:19:33 +1000

    feat(pkgs.buildtools): implement generating json

--- nix_minecraft_servers/buildtools.py
+++ nix_minecraft_servers/buildtools.py
@@ -1,18 +1,19 @@
-from typing import List
+from typing import Dict, List, Union
 from jenkins import Jenkins
 from .common import get
 from bs4 import BeautifulSoup
+import json
+from logging import getLogger
+
+
+log = getLogger(__name__)
 
 
 def last_successful_build() -> int:
     server = Jenkins("https://hub.spigotmc.org/jenkins/")
     job_info = server.get_job_info("BuildTools")
-    last_successful_build: int = job_info["last_successful_build"]["number"]
+    last_successful_build: int = job_info["lastSuccessfulBuild"]["number"]
     return last_successful_build
-
-
-def get_jar_url(build: int) -> str:
-    return f"https://hub.spigotmc.org/jenkins/job/BuildTools/{build}/artifact/target/BuildTools.jar"
 
 
 def get_versions() -> List[str]:
@@ -25,3 +26,19 @@ def get_versions() -> List[str]:
         for link in links
         if link.startswith("1.") and link.endswith(".json")
     ]
+
+
+def generate() -> Dict[str, Union[int, List[str]]]:
+    return {"build": last_successful_build(), "revs": get_versions()}
+
+
+def main() -> None:
+    with open("pkgs/buildtools.json", "w") as file:
+        data = generate()
+        log.info(f"[b]Found {len(data['revs'])} versions for BuildTools")
+        json.dump(data, file, indent=2)
+        file.write("\n")
+
+
+if __name__ == "__main__":
+    main()
