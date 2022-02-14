8ec8d48238417e3babc1b0fa05d704257d09fdba
Joel <joel@joel.tokyo>
2022-02-14 16:41:58 +1000

    perf(ci.velocity): convert to async

--- ci/minecraft_servers/velocity.py
+++ ci/minecraft_servers/velocity.py
@@ -1,28 +1,37 @@
+from asyncio import gather, run
 from logging import getLogger
 from typing import Dict, Union
 
-from .common import get_latest_major_versions
+from aiohttp import ClientSession
+
+from .common import get_latest_major_versions, get_major_release
 from .paper import Project
 
 
 log = getLogger(__name__)
 
 
+async def async_generate() -> Dict[str, Dict[str, Union[str, int]]]:
+    async with ClientSession("https://papermc.io") as session:
+        project = await Project.get(session, "velocity")
+        major_versions = get_latest_major_versions(
+            [
+                version
+                for version in project.versions
+                if not version.endswith("SNAPSHOT")
+            ]
+        ).values()
+        versions = await gather(
+            *[project.get_version(session, version) for version in major_versions]
+        )
+        builds = await gather(
+            *[version.get_build(session, max(version.builds)) for version in versions]
+        )
+
+    return {
+        get_major_release(build.version): build.output_for_nix() for build in builds
+    }
+
+
 def generate() -> Dict[str, Dict[str, Union[str, int]]]:
-    project = Project.get("velocity")
-    major_versions_str = get_latest_major_versions(
-        [version for version in project.versions if not version.endswith("SNAPSHOT")]
-    )
-    major_versions_Version = {
-        major_version: project.get_version(version)
-        for major_version, version in major_versions_str.items()
-    }
-    major_versions_Build = {
-        major_version: version.get_build(max(version.builds))
-        for major_version, version in major_versions_Version.items()
-    }
-    major_versions_dict = {
-        major_version: build.output_for_nix()
-        for major_version, build in major_versions_Build.items()
-    }
-    return major_versions_dict
+    return run(async_generate())
