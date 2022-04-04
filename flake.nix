de74a8856ec49d6c62d1dd0a1318aa7e7d01ad6b
Joel <joel@joel.tokyo>
2022-04-04 18:22:54 +1000

    fix: overlay is not system dependant
    - overlay.x86_64-linux -> overlay
    - also use flake-utils-plus

--- flake.nix
+++ flake.nix
@@ -2,30 +2,31 @@
   description = "Minecraft server packages";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs { config.allowUnfree = true; inherit system; };
-        in
+  outputs = { self, nixpkgs, utils, ... } @ inputs:
+    utils.lib.mkFlake {
+      inherit self inputs;
+
+      channelsConfig.allowUnfree = true;
+
+      outputsBuilder = channels:
+        let pkgs = channels.nixpkgs; in
         with pkgs;
-        rec {
+        {
           defaultPackage = poetry2nix.mkPoetryApplication { projectDir = ./ci; };
 
           devShell = (poetry2nix.mkPoetryEnv { projectDir = ./ci; }).env;
 
-          overlay = final: prev:
-            {
-              minecraftServers = import ./packages {
-                inherit (final) callPackage lib;
-              };
-            };
+          packages = (self.overlay pkgs pkgs).minecraftServers;
+        };
 
-          packages = (overlay pkgs pkgs).minecraftServers;
-        }
-      );
+      overlay = final: prev: {
+        minecraftServers = import ./packages {
+          inherit (final) callPackage lib;
+        };
+      };
+    };
 }
