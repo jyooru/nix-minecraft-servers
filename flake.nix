dde99a5ed87a5f4aaaea2bbdae15d5468fff39ae
Joel <joel@joel.tokyo>
2023-01-14 10:41:55 +1000

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,28 +7,30 @@
   };
 
   outputs = { self, nixpkgs, utils } @ inputs:
-    utils.lib.mkFlake {
-      inherit self inputs;
+    nixpkgs.lib.warn "nix-minecraft-servers is no longer being maintained. Please use https://github.com/Infinidoge/nix-minecraft" (
+      utils.lib.mkFlake {
+        inherit self inputs;
 
-      outputsBuilder = channels:
-        let pkgs = channels.nixpkgs; in
-        with pkgs;
-        {
-          packages = (self.overlays.default pkgs pkgs).minecraftServers // rec {
-            default = minecraft-servers;
-            minecraft-servers = callPackage ./ci { };
+        outputsBuilder = channels:
+          let pkgs = channels.nixpkgs; in
+          with pkgs;
+          {
+            packages = (self.overlays.default pkgs pkgs).minecraftServers // rec {
+              default = minecraft-servers;
+              minecraft-servers = callPackage ./ci { };
+            };
+          };
+
+        overlays = rec {
+          default = nix-minecraft-servers;
+          nix-minecraft-servers = final: prev: {
+            minecraftServers = import ./packages {
+              inherit (final) callPackage lib;
+            };
           };
         };
 
-      overlays = rec {
-        default = nix-minecraft-servers;
-        nix-minecraft-servers = final: prev: {
-          minecraftServers = import ./packages {
-            inherit (final) callPackage lib;
-          };
-        };
-      };
-
-      overlay = nixpkgs.lib.warn "the nix-minecraft-servers overlay has been renamed from 'overlay' to 'overlays.default'" self.overlays.default;
-    };
+        overlay = nixpkgs.lib.warn "the nix-minecraft-servers overlay has been renamed from 'overlay' to 'overlays.default'" self.overlays.default;
+      }
+    );
 }
